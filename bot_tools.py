27439997c5859de6150fe189becb714948422fa2
Martin Corvera Pose <ma.corv@gmail.com>
2020-02-29 10:17:29 -0300

    hotfix regex script

--- bot_tools.py
+++ bot_tools.py
@@ -2,7 +2,7 @@ import re
 
 def post_processing(raw_tweet):
     tweet = raw_tweet._json
-    match = re.search('(necesit(amos|a|an|o) (\d*)|necesit(amos|a|an|o)|buscando) (dadores) (de) (sangre)', tweet['text'])
+    match = re.search('([Nn]ecesit(amos|a|an|o) (\d*)|[Nn]ecesit(amos|a|an|o)|[Bb]uscando) (dadores) (de) (sangre)', tweet['text'])
     if match:
         try:
             id = tweet['retweeted_status']['id']
